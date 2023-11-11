c23055c196d17087cc3b8ba322c1a412566f34b9
ORSONI Nicolas <orsoni.nicolas@hotmail.fr>
2023-11-11 16:14:37 -0500

    DockerFile complete

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,6 @@
 #création des volume
 mkdir /tmp/web
 mkdir /tmp/database
-#gestion des droits sur les fichier
 
-docker build -t orsoniwebapp:0.0.1 webapp
-docker build -t orsonidatabase:0.0.1 database
+docker build -t orsoniwebapp:0.0.1 webapp -f webapp/Dockerfile
+docker build -t orsonidatabase:0.0.1 database -f database/Dockerfile
