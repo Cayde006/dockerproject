69e3e9c43503bd3a7ae1d56d4b7fe94f012b34d2
ORSONI Nicolas <orsoni.nicolas@hotmail.fr>
2023-11-13 11:58:33 +0100

    test

--- docker/setupvault.sh
+++ docker/setupvault.sh
@@ -3,4 +3,4 @@ curl \
     --header "X-Vault-Token:network" \
     --request POST \
     --data @payloadsetup.json \
-    https://127.0.0.1:8200/v1/secret/data/test
+    http://127.0.0.1:8200/v1/secret/data/test
